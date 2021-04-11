393a6792e5c0551d1ccaedd521dab44fb7e60b3d
Onyiee <elizabethonyemowo.agbo@gmail.com>
2021-04-11 16:36:10 +0100

    Main method impl

--- src/exerciseSixPointTwoNine/Coin.java
+++ src/exerciseSixPointTwoNine/Coin.java
@@ -3,4 +3,17 @@ package exerciseSixPointTwoNine;
 public enum Coin {
     HEADS,
     TAILS;
+
+
+    @Override
+    public String toString() {
+        String result ="";
+        switch (this){
+            case TAILS -> {result = "TAILS";}
+            case HEADS -> {result ="HEAD";}
+        }
+        return result;
+    }
+
+
 }
---
+++ src/exerciseSixPointTwoNine/Main.java
@@ -0,0 +1,38 @@
+package exerciseSixPointTwoNine;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CoinTossing coinTossing = new CoinTossing();
+
+        int number = 0;
+        int numberOfHeads = 0;
+        int numberOfTails = 0;
+        Coin tossedCoin;
+        Scanner input = new Scanner(System.in);
+
+        while (number != -1) {
+        System.out.println("Enter 1 to toss a coin, 2 to quit: ");
+        number = input.nextInt();
+
+        if (number == 1) {
+            tossedCoin = coinTossing.flip();
+            System.out.println(tossedCoin);
+            if (tossedCoin == Coin.HEADS){
+                numberOfHeads ++;
+            }
+            if (tossedCoin == Coin.TAILS){
+                numberOfTails ++;
+            }
+        }else {
+            System.out.println("You have ended the program...");
+        }
+
+        }
+        System.out.println("number of heads is " + numberOfHeads);
+        System.out.println("Number of tails is " + numberOfTails);
+    }
+}
