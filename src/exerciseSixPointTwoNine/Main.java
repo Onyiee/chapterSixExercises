5b48fa97072c36a8e3d7425c047771f6a3b0b9a0
Onyiee <elizabethonyemowo.agbo@gmail.com>
2021-04-11 23:06:36 +0100

    handled exceptions

--- src/exerciseSixPointTwoNine/Main.java
+++ src/exerciseSixPointTwoNine/Main.java
@@ -1,10 +1,11 @@
 package exerciseSixPointTwoNine;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         CoinTossing coinTossing = new CoinTossing();
 
@@ -14,25 +15,30 @@ public class Main {
         Coin tossedCoin;
         Scanner input = new Scanner(System.in);
 
-        while (number != -1) {
-        System.out.println("Enter 1 to toss a coin, 2 to quit: ");
-        number = input.nextInt();
+        try {
+            while (number != -1) {
+                System.out.println("Enter 1 to toss a coin, 2 to quit: ");
+                number = input.nextInt();
 
-        if (number == 1) {
-            tossedCoin = coinTossing.flip();
-            System.out.println(tossedCoin);
-            if (tossedCoin == Coin.HEADS){
-                numberOfHeads ++;
+                if (number == 1) {
+                    tossedCoin = coinTossing.flip();
+                    System.out.println(tossedCoin);
+                    if (tossedCoin == Coin.HEADS){
+                        numberOfHeads ++;
+                    }
+                    if (tossedCoin == Coin.TAILS){
+                        numberOfTails ++;
+                    }
+                }else {
+                    System.out.println("You have ended the program...");
+                }
+
             }
-            if (tossedCoin == Coin.TAILS){
-                numberOfTails ++;
-            }
-        }else {
-            System.out.println("You have ended the program...");
+            System.out.println("number of heads is " + numberOfHeads);
+            System.out.println("Number of tails is " + numberOfTails);
+        }catch (InputMismatchException e){
+            System.out.println("You need to enter a number");
         }
 
-        }
-        System.out.println("number of heads is " + numberOfHeads);
-        System.out.println("Number of tails is " + numberOfTails);
     }
 }
--- test/exerciseSixPointTwoNine/CoinTossingTest.java
+++ test/exerciseSixPointTwoNine/CoinTossingTest.java
@@ -1,4 +1,37 @@
+package exerciseSixPointTwoNine;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CoinTossingTest {
-  
+    CoinTossing coinTossing;
+    Coin coin;
+
+    @BeforeEach
+    void setUp() {
+        coinTossing = new CoinTossing();
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void coinCanBeSetAndGotten(){
+        coinTossing.setCoin(coin);
+        assertEquals(coin, coinTossing.getCoin());
+    }
+
+    @Test
+    void coinCanBeTossed(){
+        coinTossing.setCoin(Coin.TAILS);
+        assertNotNull(coinTossing.flip());
+        coinTossing.setCoin(Coin.HEADS);
+        assertNotNull(coinTossing.flip());
+    }
+
 }
